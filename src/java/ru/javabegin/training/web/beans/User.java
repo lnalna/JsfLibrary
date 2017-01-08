4e884349d6f3874868260186491c1cca5c981699
lnalna <lnalna@yandex.ru>
2017-01-08 19:03:25 +0300

    Начало проекта

---
+++ src/java/ru/javabegin/training/web/beans/User.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.javabegin.training.web.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author nik
+ */
+
+@ManagedBean
+@SessionScoped
+
+public class User {
+    
+}
