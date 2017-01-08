f0aa54ffb54acbf179b057da17311c0c7e2375a8
lnalna <lnalna@yandex.ru>
2017-01-08 19:14:32 +0300

    Создание класса User - управляемый бин. Создание переменной username, пустого конструктора, геттера, сеттера.

--- src/java/ru/javabegin/training/web/beans/User.java
+++ src/java/ru/javabegin/training/web/beans/User.java
@@ -18,4 +18,17 @@ import javax.faces.bean.SessionScoped;
 
 public class User {
     
+    private String username;
+    
+    public User(){
+        
+    }
+    
+    public String getUsername(){
+        return username;
+    }
+    
+    public void setUsername(String username){
+        this.username = username;
+    }
 }
