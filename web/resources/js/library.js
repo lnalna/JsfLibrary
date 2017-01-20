6ac93c3ff1ecc0ec4f69e548ae2d4c4d38d088c3
lnalna <lnalna@yandex.ru>
2017-01-20 15:13:48 +0300

    создание функции проверки имени пользователя

---
+++ web/resources/js/library.js
@@ -0,0 +1,14 @@
+function checkValue(form, message){
+    
+    var userInput = form[form.id + ":username"];
+    
+    if (userInput.value ==''){
+        alert(message)
+        userInput.focus();
+        return false;
+    }
+    
+    return true;
+}
+
+
