42e19f5f653ebef365079b5535ddfa489f359338
Nikolay <lnalna@yandex.ru>
2017-01-23 11:46:33 +0300

    создание отдельного файла с настройками локали locales.xhtml подключение локали на стартовой странице index.xhtml подключение локали на странице header.xhtml

--- src/java/ru/javabegin/training/web/beans/LocalChanger.java
+++ src/java/ru/javabegin/training/web/beans/LocalChanger.java
@@ -4,14 +4,14 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import java.util.Locale;
 import java.io.Serializable;
-import javax.faces.context.FacesContext;
+//import javax.faces.context.FacesContext;
 
 
-@ManagedBean
+@ManagedBean(eager=true)
 @SessionScoped
 public class LocalChanger implements Serializable{
     
-    private Locale currentLocale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+    private Locale currentLocale = new Locale("ru");//FacesContext.getCurrentInstance().getViewRoot().getLocale();
     
     public LocalChanger(){
         
