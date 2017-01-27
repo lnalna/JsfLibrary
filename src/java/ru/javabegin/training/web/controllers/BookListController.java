df999bdfead7c5dc95a8b431b0967fe7acac1e24
lnalna <lnalna@yandex.ru>
2017-01-27 12:34:48 +0300

    рефакторинг метода получения русских букв  getRussianLetters

--- src/java/ru/javabegin/training/web/controllers/BookListController.java
+++ src/java/ru/javabegin/training/web/controllers/BookListController.java
@@ -406,45 +406,13 @@ public class BookListController implements Serializable{
         
     }
     
+//<editor-fold defaultstate="collapsed" desc="получение всего русского алфавита getRussianLetters">
     public Character[] getRussianLetters() {
-        Character[] letters = new Character[33];
-        letters[0] = 'А';
-        letters[1] = 'Б';
-        letters[2] = 'В';
-        letters[3] = 'Г';
-        letters[4] = 'Д';
-        letters[5] = 'Е';
-        letters[6] = 'Ё';
-        letters[7] = 'Ж';
-        letters[8] = 'З';
-        letters[9] = 'И';
-        letters[10] = 'Й';
-        letters[11] = 'К';
-        letters[12] = 'Л';
-        letters[13] = 'М';
-        letters[14] = 'Н';
-        letters[15] = 'О';
-        letters[16] = 'П';
-        letters[17] = 'Р';
-        letters[18] = 'С';
-        letters[19] = 'Т';
-        letters[20] = 'У';
-        letters[21] = 'Ф';
-        letters[22] = 'Х';
-        letters[23] = 'Ц';
-        letters[24] = 'Ч';
-        letters[25] = 'Ш';
-        letters[26] = 'Щ';
-        letters[27] = 'Ъ';
-        letters[28] = 'Ы';
-        letters[29] = 'Ь';
-        letters[30] = 'Э';
-        letters[31] = 'Ю';
-        letters[32] = 'Я';
-
+        Character[] letters = new Character[]{'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я',};
         return letters;
     }
     
+//</editor-fold>
     
 //<editor-fold defaultstate="collapsed" desc="геттеры и сеттеры">
     public String getSearchString() {
@@ -525,6 +493,7 @@ public class BookListController implements Serializable{
     }
 //</editor-fold>
     
+//<editor-fold defaultstate="collapsed" desc="имитация загрузки из базы данных метод imitateLoading">
     private void imitateLoading() {
         try {
             Thread.sleep(1000);// имитация загрузки процесса
@@ -532,4 +501,5 @@ public class BookListController implements Serializable{
             Logger.getLogger(BookListController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+//</editor-fold>
 }
