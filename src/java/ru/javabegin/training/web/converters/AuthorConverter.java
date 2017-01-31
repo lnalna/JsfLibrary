dffde42dbfd0b03af5e15ea22795e4d67d00071a
Nikolay <lnalna@yandex.ru>
2017-01-31 16:17:33 +0300

    создан класс AuthorConverter

---
+++ src/java/ru/javabegin/training/web/converters/AuthorConverter.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.javabegin.training.web.converters;
+
+/**
+ *
+ * @author nik
+ */
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import ru.javabegin.training.web.controllers.AuthorController;
+import ru.javabegin.training.web.db.DataHelper;
+
+public class AuthorConverter implements Converter {
+
+    private static AuthorController authorController;
+
+    public AuthorConverter() {
+        authorController = (AuthorController) FacesContext.getCurrentInstance().getExternalContext().getApplicationMap().get("authorController");
+    }
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        return DataHelper.getInstance().getAuthor(Long.valueOf(value));
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        System.out.println(value);
+        return "";//((Author)value).getFio();
+    }
+}
