94de0a176442963f3d52815941205820b2a8df78
Nikolay <lnalna@yandex.ru>
2017-02-02 17:50:57 +0300

    Изменение HibernateUtil

--- src/java/ru/javabegin/training/web/entity/HibernateUtil.java
+++ src/java/ru/javabegin/training/web/entity/HibernateUtil.java
@@ -5,8 +5,11 @@
  */
 package ru.javabegin.training.web.entity;
 
-import org.hibernate.cfg.AnnotationConfiguration;
+//import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
 
 /**
  * Hibernate Utility class with a convenient method to get Session Factory
@@ -14,6 +17,7 @@ import org.hibernate.SessionFactory;
  *
  * @author nik
  */
+/*
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory;
@@ -33,4 +37,50 @@ public class HibernateUtil {
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
+  */  
+/*
+public class HibernateUtils {
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static ServiceRegistry serviceRegistry;
+
+    private static SessionFactory buildSessionFactory() {
+
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties()).build();
+        return configuration.buildSessionFactory(serviceRegistry);
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
+*/
+    public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+    private static ServiceRegistry serviceRegistry;
+
+    static{
+        try {
+
+               Configuration configuration = new Configuration();
+               configuration.configure();
+               serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+               sessionFactory=configuration.buildSessionFactory(serviceRegistry);
+        }catch (Throwable ex) {
+            // Log the exception. 
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+
+    
 }
